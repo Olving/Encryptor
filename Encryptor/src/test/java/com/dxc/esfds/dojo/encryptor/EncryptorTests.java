ac0c0232c888ee050708298ee1a816ec52371113
JosepPC <JosepPC@HCES5CG54924L4>
2018-03-09 11:15:12 +0100

    [Olving] initial commit

---
+++ Encryptor/src/test/java/com/dxc/esfds/dojo/encryptor/EncryptorTests.java
@@ -0,0 +1,16 @@
+package com.dxc.esfds.dojo.encryptor;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+public class EncryptorTests {
+
+	@Test
+	public void test() {
+		assertThat(true, is(true));
+	}
+
+}
+
